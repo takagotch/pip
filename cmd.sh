bfac91609fbcb7428044040d5a33f88942c500a3
takagotch <dyaccb@gmail.com>
2018-02-21 19:13:07 +0900

    pip install pillow qrcode

---
+++ cmd.sh
@@ -0,0 +1,7 @@
+pip install pillow qrcode
+pip install -U pip
+python3 qrcode-fr.py
+pip uninstall pillow qrcode
+
+pip install pycrpyto
+
---
+++ qrcode-fr.py
@@ -0,0 +1,4 @@
+import qrcode
+img = qrcode.make("http://tk.com")
+img.save("qrcode-fr.png")
+
